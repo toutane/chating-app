875452ac0d5653dcd7bd25c11ac002cfd9c10479
Charles-Antoine LEGER <ca@leger.email>
2020-02-27 23:26:53 +0100

    try some bottom bar UI...

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -24,24 +24,54 @@ function Auth() {
 function Routes() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: false,
+          inactiveTintColor: "#333333",
+          activeTintColor: "#1DC161"
+        }}
+      >
         <Tab.Screen
-          name="Chats"
+          name="Notifications"
           component={Chats}
           options={{
-            tabBarLabel: "Chats",
+            tabBarLabel: "Notifications",
             tabBarIcon: ({ color }) => (
-              <Feather name="message-circle" size={20} color={color} />
+              <Feather name="bell" size={25} color={color} />
             )
           }}
         />
         <Tab.Screen
-          name="Auth"
+          name="Feed"
+          component={Chats}
+          options={{
+            tabBarLabel: "Feed",
+            tabBarIcon: ({ color }) => (
+              <Feather name="grid" size={25} color={color} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
           component={Auth}
           options={{
-            tabBarLabel: "User",
+            tabBarLabel: "Profile",
             tabBarIcon: ({ color }) => (
-              <Feather name="user" size={20} color={color} />
+              <Image
+                style={{
+                  width: 30,
+                  height: 30,
+                  borderRadius: 50,
+                  borderWidth: 2,
+                  borderColor: color
+                }}
+                source={{
+                  uri:
+                    "https://scontent-cdt1-1.cdninstagram.com/v/t51.2885-19/s320x320/80847132_1150219625428179_1223222822092931072_n.jpg?_nc_ht=scontent-cdt1-1.cdninstagram.com&_nc_ohc=QRo_CYtfBGIAX8jBYkg&oh=3fa73a07ebc74ed97a0640a90e5dc2ed&oe=5E89B572"
+                  // "https://scontent-cdt1-1.cdninstagram.com/v/t51.2885-19/s320x320/70639440_499542157443515_3701313669351604224_n.jpg?_nc_ht=scontent-cdt1-1.cdninstagram.com&_nc_ohc=YfscEbN7wbkAX-vFG5v&oh=f95ec4ffef70650e15eb936ac99508e2&oe=5E871D55"
+                }}
+              />
+              /* <Feather name="circle" size={25} color={color} /> */
             )
           }}
         />
